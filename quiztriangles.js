2af248187e5ea9f6c0463cd7e6cdb60cc9608f6b
ahsan-rigu <114474110+ahsan-rigu@users.noreply.github.com>
2022-10-28 19:01:05 +0530

    final

--- quiztriangles.js
+++ quiztriangles.js
@@ -1,13 +1,12 @@
 const quizForm = document.querySelector("#quizForm");
-const checkAnswerBtn = document.querySelector("#check-answers");
-const outputBox = document.querySelector(".output-box")
+const checkAnswerButton = document.querySelector("#check-answers");
+const outputBox = document.querySelector(".output-box");
 
 correctAnswers = ["Equillateral", "Isoceless", "Both a. and b."]
 
-checkAnswerBtn.addEventListener("click", console.log(23));
+checkAnswerButton.addEventListener("click" , checkAnswers);
 
 function checkAnswers(){
-    console.log(23);
     var score = 0;
     var index = 0;
     var formResults = new FormData(quizForm);
